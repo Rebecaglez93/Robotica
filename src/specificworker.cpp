c811dd6607451bcd8ea22f3b5a28eccf2492920b
Rebeglez93 <rebcaglez22@gmail.com>
2014-10-07 17:35:49 +0200

    subida sesion 4 apriltags

--- src/specificworker.cpp
+++ src/specificworker.cpp
@@ -18,6 +18,7 @@
  */
  
  #include "specificworker.h"
+#include <qt4/QtCore/QDate>
 
 /**
 * \brief Default constructor
@@ -25,6 +26,7 @@
 
 SpecificWorker::SpecificWorker(MapPrx& mprx, QObject *parent) : GenericWorker(mprx, parent)	
 {
+	s = STATE::INITAVANZAR;
 }
 
 /**
@@ -34,24 +36,165 @@ SpecificWorker::~SpecificWorker()
 {
 
 }
+
 void SpecificWorker::compute( )
 {
-	TLaserData laser_data = laser_proxy->getLaserData(); // Distancia laser
-	//robot = differentialrobot_proxy->setSpeedBase();
-	try {
-		for(auto i: laser_data){
-			if(i.dist<100){
-				differentialrobot_proxy->stopBase();
-				qDebug()<<"Para";
-				break;
-			}
-		}
-	} catch (const Ice::Exception &E) {
-		std::cout<< E << endl;
+	laser_data=laser_proxy->getLaserData();
+	switch(s)
+	{
+		case STATE::INITAVANZAR:
+			initAvanzar();
+			break;
+		case STATE::AVANZAR:
+			avanzar();
+			break;
+		case STATE::PARAR:
+			parar();
+			break;
+		case STATE::INITGIRAR:
+			initGirar();
+			break;
+		case STATE::GIRAR:
+			girar();
+			break;
+		case STATE::CONTROL:
+			control();
+			break;
+		case STATE::IDLE:
+			
+			break;
 	}
 }
+
+bool SpecificWorker::initAvanzar()
+{
+	if(estaCerca()){
+		s=STATE::INITGIRAR;
+		return true;
+	}else{
+		try {
+			differentialrobot_proxy->setSpeedBase(300,0);
+			s=STATE::AVANZAR;
+			qDebug()<<"Estado InitAvanzar";
+		} catch (const Ice::Exception &E) {
+			std::cout<< E << endl;
+		}
+		return false;
+	}
+}
+
+
+
+void SpecificWorker::avanzar(){
+	qDebug()<<"Estado Avanzar";
+	if( estaCerca() )
+			s=STATE::PARAR;
+
+}
+
+
+bool SpecificWorker:: leerMarca( int id){
+/*	
+	for(auto i: tagslocal){
+	  
+		if(i.id==id){
+			
+			return true;
+	}
+	
+}
+
+return false;*/
+}
+
+
+
+void SpecificWorker::parar(){
+	
+	try {
+		differentialrobot_proxy->setSpeedBase(0,0);
+		qDebug()<<"Estado Parar";
+		s=STATE::INITGIRAR;
+		} catch (const Ice::Exception &E) {
+			std::cout<< E << endl;
+		}
+}
+
+void SpecificWorker::initGirar()
+{
+        try 
+        {
+           differentialrobot_proxy->setSpeedBase ( 0,0.7 );
+           s=STATE::GIRAR;
+          qDebug() <<"Estado InitGirar";
+        } catch ( const Ice::Exception &E ) 
+		{   std::cout<< E << endl; }
+}
+
+bool SpecificWorker::girar(){
+	
+	tag t;
+	if(tagslocal.existsId(3,t))
+	{
+		parar();
+		s=STATE::CONTROL;
+		return true;
+	}
+}
+
+bool SpecificWorker::control()
+{
+	const float P= 1;
+	const float Q= 1;
+	
+	tag t;
+	if(tagslocal.existsId(3, t))
+	{
+			if(t.tz<300)
+			{
+				s=STATE::PARAR;
+				return false;
+			
+			}
+			
+			float vrot=t.tx * P;
+			if( vrot > 0.8) vrot = 0.8;
+			if( vrot < -0.8) vrot = -0.8;
+			float vadv=t.tz * Q;
+			if(vadv > 600) vadv = 600;
+			try 
+			{
+				differentialrobot_proxy->setSpeedBase ( 0,0.7 );
+				s=STATE::GIRAR;
+				qDebug() <<"Estado InitGirar";
+        } catch ( const Ice::Exception &E )	{   std::cout<< E << endl; }
+		
+		}
+		return true;
+}
+
 bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
 {
-	timer.start(Period);
-	return true;
+    timer.start(Period);
+    return true;
 };
+ 
+
+
+bool SpecificWorker::estaCerca(){
+	qDebug()<<"Tamaño del array"<<laser_data.size();
+	for(int i=1; i<laser_data.size();i++){
+		qDebug()<<"Distancia laser"<<laser_data[i].dist;
+		if(laser_data[i].dist<500 and fabs(laser_data[i].angle) < 1.2){
+			return true;
+		}
+	}
+	return false;
+}
+
+void SpecificWorker::newAprilTag(const tagsList& tags)
+{
+	
+	tagslocal.update( tags) ;
+	
+}
--- src/specificworker.h
+++ src/specificworker.h
@@ -29,13 +29,71 @@
 class SpecificWorker : public GenericWorker
 {
 Q_OBJECT
+private:
+	bool girando = false;
+	enum class STATE {INITAVANZAR, AVANZAR, PARAR, INITGIRAR, GIRAR, CONTROL, IDLE};
+	STATE s;
+	QTime reloj;
+	TLaserData laser_data;
+	
+	struct tag
+	{
+	 int id;
+	 float tx,tz,ry;	
+	 tag(){};
+	 tag( int id_, float tx_, float tz_, float ry_)
+	 {
+		 tx = tx_*1000; tz = tz_*1000; ry = ry_; id = id_;
+	 }
+	};
+	
+	struct tagslocalT
+	{
+		QMutex mutex;
+		void update( const tagsList &t)
+		{
+			mutex.lock();
+			tags.clear();
+			for(auto i: t)
+			{
+				tag myT(i.id, i.tx, i.tz, i.ry);
+				tags.push_back(myT);
+			}
+			mutex.unlock();
+		};
+		bool existsId(int id_, tag &tt)
+		{
+			mutex.lock();
+			for(auto i: tags)
+			if( i.id == id_)
+			{
+				tt=i;
+				return true;
+			}
+			mutex.unlock();
+			return false;
+		}
+		std::vector<tag> tags;
+	};
+	
+	tagslocalT tagslocal;
+	
 public:
 	SpecificWorker(MapPrx& mprx, QObject *parent = 0);	
 	~SpecificWorker();
 	bool setParams(RoboCompCommonBehavior::ParameterList params);
+	void initGirar();
+	bool initAvanzar();
+	void avanzar();
+	bool girar();
+	void parar();
+	bool estaCerca();
+	bool control();
+	void  newAprilTag(const tagsList& tags);
+	bool leerMarca( int id);
 
 public slots:
- 	void compute(); 	
+ 	void compute(); 
 };
 
 #endif
